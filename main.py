86d7eb1a90bce13c8c4d15c32baa7d38010cd134
seanjosefosky <josefoskysean@gmail.com>
2025-01-19 19:05:09 -0600

    Updated some functions and organization

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
-# Example file showing a circle moving on screen
 import pygame, random as r
-
+from pygame import gfxdraw
+import pygame.gfxdraw
+import rocket, simulation
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1200, 720))
+screen_w = screen.get_width
+screen_h = screen.get_height
 clock = pygame.time.Clock()
 running = True
 dt = 0
-
-# Bodies Handling
-
-bodies = []
-
-class Bodies:
-    def __init__(self, name, pos, radius):
-        self.name = name
-        self.pos = pos
-        self.radius = radius
-
-
-def DrawBodies():
-    limit = 100
-    if len(bodies) < limit:
-        radius = r.uniform(0.1,3)
-        bodyspawn = pygame.Vector2(r.uniform(0,screen.get_width()), r.uniform(0,screen.get_height()))
-        newbody = Bodies(len(bodies), bodyspawn, radius)
-        bodies.append(newbody)
-
-    # Draw "bodies" list after all of their information is generated
-    if len(bodies) == limit:
-        for body in bodies:
-            pygame.draw.circle(screen, "white", body.pos, body.radius)
-
-
-# TODO: Prevent body collision when spawning
-
 
 # TODO: Drag screen with mouse
 def MoveScreen():
@@ -44,18 +19,73 @@ def MoveScreen():
         print(Mouse_x,Mouse_y)
 
 
+# Background Handling
+stars = []
+class Stars:
+    def __init__(self, name, pos, radius):
+        self.name = name
+        self.pos = pos
+        self.radius = radius
+
+def DrawStars():
+    limit = 100
+    if len(stars) < limit:
+        radius = r.uniform(0.1,3)
+        spawn = pygame.Vector2(r.uniform(0,screen.get_width()), r.uniform(0,screen.get_height()))
+        newstar = Stars(len(stars), spawn, radius)
+        stars.append(newstar)
+
+    # Draw all stars in 'stars' list after all of their information is created
+    if len(stars) == limit:
+        for body in stars:
+            pygame.draw.circle(screen, "white", body.pos, body.radius)
+
+    # TODO: Prevent stars overlapping when spawning
+
+
+# Draw it antialiased
+class MainBody:
+    m = 100
+    rad = 60
+    pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+def DrawMainBody():
+    body = MainBody()
+    color = pygame.Color(48,41,191)
+    x = int(body.pos.x)
+    y = int(body.pos.y)
+    pygame.gfxdraw.aacircle(screen, x, y, body.rad, color)
+    pygame.gfxdraw.filled_circle(screen, x, y, body.rad, color)
+
+    #TODO: Make circle look like sphere
+    # pygame.gfxdraw.arc(screen, 300, 100, 60, 45, 180, color)
+
+
+
+# TODO #
+# Newtonian gravity simulation
+# mass, distance, scale, velocity
+# zone of influence (2 body simulation)
+# 
+
+
+# TODO: Draw orbit path behind thing
+
+
 
 # Game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    simulation.Simulation(MainBody,rocket.Rocket())
 
+    screen.fill("black")
 
-        screen.fill("black")
-
-        DrawBodies()
-        MoveScreen()
+    DrawStars()
+    # MoveScreen()
+    DrawMainBody()
+    rocket.DrawRocket(screen, 600, 500)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
